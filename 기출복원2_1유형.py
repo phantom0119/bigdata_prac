dd7e31fbde3235352b0e628b30f29db9fd472a90
phantom0119 <phantom0119@daum.net>
2024-11-09 14:36:54 +0900

    feat: 빅분기 실기 준비 제대로

--- 기출복원2_1유형.py
+++ 기출복원2_1유형.py
@@ -16,3 +16,30 @@ newdf['crim'].iloc[0:10] = rev
 
 q3 = newdf[newdf['age'] >= 80]
 print(q3.crim.mean())
+
+
+#-------------------------------------------------------------------------
+
+df = pd.read_csv('./workpython/housing.csv')
+
+r = int(len(df)*0.8)  #16512.0
+
+data1 = df.iloc[0:r]
+
+t1 = data1['total_bedrooms'].dropna()
+t2 = data1['total_bedrooms'].fillna(data1['total_bedrooms'].median())
+
+print(t1.std(), t2.std())
+
+
+#------------------------------------------------------------------------
+
+df = pd.read_csv('./workpython/insurance.csv')
+print(df.head())
+m = df['charges'].mean()
+n = df['charges'].std()
+outlier = m + 1.5*n
+
+tmp = df[df['charges'] >= outlier]
+tmps = tmp['charges'].sum()
+print(m, n, outlier, tmps)
